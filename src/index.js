78ff74dba40fb36acb5e9acf07d1e2f68a057156
Pranaya Bhat <prbhat@qca-sdc.com>
2018-09-22 13:03:24 -0500

    chore:add style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import css from "./style.css";
+//import styles from "./style.css";
+
 const contain = document.querySelector("#git-form");
 const user = contain.querySelector("#git-search-input");
 
-const API_KEY = "7626b6c20290bf144b1a3283acc48d5e76e7b453";
+const API_KEY = "72d2d6f54dfbd95ab937b61627e77a2fe05485a9";
 
 const fetchGifs = url => {
   return fetch(url).then(res => res.json());
@@ -14,11 +17,11 @@ const gitCollection = () => {
     fetchGifs(
       `https://api.github.com/users/${username}/repos?access_token=${API_KEY}`
     ).then(data => {
-      console.log(data[0].name);
-      data.filter(data => {
+      console.log(data.name);
+      data.map(data2 => {
         var para = document.createElement("p");
         const temp = document.querySelector("#container2");
-        para.innerText = data.name;
+        para.innerText = data2.name;
         temp.appendChild(para);
       });
     });

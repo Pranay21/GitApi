479ce8afaae9150b28ccbfb3ef67c2df1fc2c9bb
Pranaya Bhat <prbhat@qca-sdc.com>
2018-09-24 09:31:11 -0500

    feat:add error handling

--- src/list.js
+++ src/list.js
@@ -1,12 +1,22 @@
-const API_KEY = "b6617accdf9160a38f4b748a439b0975291a55ff";
+const API_KEY = "8fe4124e6f202d14f28abe30f05f167c1252ccc2";
 
 const contain = document.querySelector("#git-form");
 const contain2 = document.querySelector("#container2");
 const user = contain.querySelector("#git-search-input");
 
 const fetchGifs = url => {
-  return fetch(url).then(res => res.json());
+  return fetch(url)
+    .then(res => res.json())
+    .catch(displayError);
 };
+
+function displayError(err) {
+  const errorP = document.createElement("p");
+  errorP.innerText = err.message;
+
+  contain2.innerHTML = "";
+  contain2.appendChild(errorP);
+}
 
 export const gitCollection = () => {
   contain.addEventListener("submit", e => {
